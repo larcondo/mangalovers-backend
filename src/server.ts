a13b09378d6908fa345bd0de23c8c6593046f53c
larcondo <larcondo90@gmail.com>
2026-04-14 15:08:50 -0300

    Modify server starting to allow better testing

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,27 @@
 import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "@schema/index";
 import { resolvers } from "@resolvers/index";
+import { JWTService } from "@services/jwt";
 
 export const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+export async function startApolloServer(port: number = 4000) {
+  const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => {
+      const auth = req ? req.headers.authorization : null;
+      const currentUser =
+        auth && auth.startsWith("Bearer ")
+          ? JWTService.verifyAccessToken(auth.substring(7))
+          : null;
+      return { currentUser };
+    },
+    listen: {
+      port,
+    },
+  });
+  return { url, server };
+}

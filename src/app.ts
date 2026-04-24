1e2e618bad2f63db3ebe3af0dd425be3f50b3e1a
larcondo <larcondo90@gmail.com>
2026-04-24 15:42:11 -0300

    Add express, as-integration and cors, and adapt server to express/grapqhl

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Mangalovers con express y apollo-server.</h1><h2>Version: 1.0.0</h2>",
+  );
+});
+
+app.get("/version", (_, res) => {
+  res.json({
+    name: "Mangalovers",
+    version: "1.0.0",
+  });
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import "dotenv/config";
-import { startApolloServer } from "./server";
 import logger from "@services/logger";
 import config from "@config/config";
+import { server, httpServer } from "./server";
+import app from "./app";
+import { expressMiddleware } from "@as-integrations/express5";
+import { apolloContext } from "./context";
 
 const PORT = config.PORT;
 
-try {
-  const { url } = await startApolloServer(PORT);
-  logger.log(`Mangalovers Server ready at ${url}`);
-} catch (err) {
-  console.log(err);
-  process.exit(1);
-}
+// We must call and await server.start() before using the integration.
+await server.start();
+
+// Set up our Express middleware to handle graphql requests
+app.use(
+  "/graphql",
+  expressMiddleware(server, {
+    context: apolloContext,
+  }),
+);
+
+httpServer.listen(PORT, () => {
+  logger.log(`Mangalovers Server running on port: ${PORT}`);
+  logger.log(`Sandbox Apollo Server on: http://localhost:${PORT}/graphql`);
+  logger.log(`Started at: ${new Date()}`);
+});
